9e2f04ed43949bd9786bbd70f9a672f1205f2ada
RoofAlan <m15024617172@163.com>
2025-01-03 17:44:22 +0800

    Updated astrknl.h

--- include/astrknl.h
+++ include/astrknl.h
@@ -13,7 +13,7 @@
 #define BUILD_DATE __DATE__
 #define BUILD_TIME __TIME__
 #define KERNL_VERS "v0.96-ArchLinux-beta"
-#define KERNL_BUID 2025-13+51
+#define KERNL_BUID 2025-13+1744
 
 /* 内核开关 */
 //#define DEBUG_SHELL
